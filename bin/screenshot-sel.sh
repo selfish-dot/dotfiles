29b52fce16544058cc2223f25e9662bcf8a4d7d8
selfish-dot <hectorjulio45676@gmail.com>
2025-11-18 19:51:20 +0000

    Add files via upload

---
+++ bin/screenshot-sel.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR="/home/$USER/Pictures/screenshots"
+FILE="$DIR/$(date +%d-%m-%Y-%H-%M-%S).png"
+
+# Make sure the directory exists
+mkdir -p "$DIR"
+
+# Take the screenshot and copy it to clipboard
+scrot -s "$FILE" && \
+xclip -selection clipboard -t image/png -i "$FILE" && \
+notify-send "Screenshot Saved & Copied" "$FILE" --icon="$FILE" --hint=int:transient:1
+
